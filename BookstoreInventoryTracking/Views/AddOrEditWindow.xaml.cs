43e4be1e6ad993eaa65d06f109baa569cfddc0b7
Ömer Faruk Çörekci <omerfarukcorekci@gmail.com>
2024-12-22 02:10:28 +0300

    Add .gitignore file

---
+++ BookstoreInventoryTracking/Views/AddOrEditWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace BookstoreInventoryTracking.Views;
+
+public partial class AddOrEditWindow : Window
+{
+    public AddOrEditWindow()
+    {
+        InitializeComponent();
+    }
+}
