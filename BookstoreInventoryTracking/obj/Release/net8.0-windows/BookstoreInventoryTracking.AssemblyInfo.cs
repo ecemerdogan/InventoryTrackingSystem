ff7e64a7278a48a68632bd44dca575a1f76fa26a
unknown <erdoganec20@itu.edu.tr>
2025-01-13 15:59:29 +0300

    Uploaded images from Bookstore Inventory Tracking System.

--- BookstoreInventoryTracking/obj/Release/net8.0-windows/BookstoreInventoryTracking.AssemblyInfo.cs
+++ BookstoreInventoryTracking/obj/Release/net8.0-windows/BookstoreInventoryTracking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreInventoryTracking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64711fe1c0d07786b3dbf24bb4b461f76299057a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac1368ca200625f6b1083bb55afd0a1ae96af30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreInventoryTracking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreInventoryTracking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
